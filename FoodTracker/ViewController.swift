be290afdac276206fba0be2ea76061864db8cfc0
webbwebb2 <webbli@bu.edu>
2016-10-01 04:52:29 -0400

    Build UI start

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Webb on 01/10/2016.
+//  Copyright © 2016 Bai Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
